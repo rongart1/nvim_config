c5a19f0d215fa5b85ef06dd504a143573e99e0cf
rongart1 <rongartenbank@gmail.com>
2025-06-11 18:50:47 +0300

    added better java run

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -66,22 +66,40 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- keep these at top-level so they persist across <leader>r invocations
+local java_winid, java_bufnr
+
 vim.keymap.set('n', '<leader>r', function()
+  -- if there’s an old run open, close its window + delete its buffer
+  if java_winid and vim.api.nvim_win_is_valid(java_winid) then
+    vim.api.nvim_win_close(java_winid, true)
+  end
+  if java_bufnr and vim.api.nvim_buf_is_valid(java_bufnr) then
+    vim.api.nvim_buf_delete(java_bufnr, { force = true })
+  end
+
+  -- save current file
+  vim.cmd 'write'
+
+  -- determine package & class
   local filepath = vim.fn.expand '%:p'
-  local filename = vim.fn.expand '%:t'
   local classname = vim.fn.expand '%:t:r'
   local dir = vim.fn.expand '%:p:h'
-  local package = nil
-
+  local pkg
   for line in io.lines(filepath) do
-    local pkg = line:match '^%s*package%s+([%w%.]+)%s*;'
-    if pkg then
-      package = pkg
+    local m = line:match '^%s*package%s+([%w%.]+)%s*;'
+    if m then
+      pkg = m
       break
     end
   end
+  local class_to_run = (pkg and pkg .. '.' .. classname) or classname
 
-  local class_to_run = package and (package .. '.' .. classname) or classname
-
-  vim.cmd('vsplit | terminal cd ' .. dir .. ' && javac -d . ' .. filename .. ' && java ' .. class_to_run)
+  -- open new split + run
+  vim.cmd 'w'
+  vim.cmd 'vsplit'
+  java_winid = vim.api.nvim_get_current_win()
+  vim.cmd('terminal cd ' .. dir .. ' && javac -d . *.java && java ' .. class_to_run)
+  java_bufnr = vim.api.nvim_get_current_buf()
 end, { desc = 'Run current Java file (with package)' })
